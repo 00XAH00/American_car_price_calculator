9c574edd9402af1fa6ef00cd081a062aa0fc08cb
00XAH00 <pxaxulin@gmail.com>
2023-03-20 18:34:10 +0300

    add validate file type

--- web/callbacks/upload_file.py
+++ web/callbacks/upload_file.py
@@ -5,14 +5,19 @@ from app import app
 
 @app.callback(Output('output-image-upload', 'children'), Input('upload-image', 'contents'),
               State('upload-image', 'filename'), State('upload-image', 'last_modified'), log=True)
-def update_output(list_of_contents, list_of_names, list_of_dates, dash_logger: DashLogger):
-    if list_of_contents is not None:
-        print(list_of_contents)
-        print(list_of_names)
-        print(list_of_dates)
-        dash_logger.info("test :)", autoClose=5000)
-        # children = [
-        #     parse_contents(list_of_contents, list_of_names, list_of_dates)
-        # ]
-        # print(children)
+def update_output(contents, name, dates, dash_logger: DashLogger):
+    if contents is not None:
+        dash_logger.info("Файл получен:)", autoClose=5000)
+        if not type_validate(name):
+            dash_logger.warning("Файл должен быть в формате csv!", autoClose=5000)
         return html.Div("test")
+
+
+def type_validate(file_name: str) -> bool:
+    split_name = file_name.split(".")
+    file_format = split_name[-1]
+
+    if file_format == "csv":
+        return True
+
+    return False
