ea1afde1958618ce39f8f18d4782afbd1280350a
00XAH00 <pxaxulin@gmail.com>
2023-03-20 18:33:25 +0300

    fix btn Input in callback for change menu

--- web/callbacks/change_menu_page.py
+++ web/callbacks/change_menu_page.py
@@ -6,9 +6,9 @@ from dash_extensions.enrich import Input, Output, ctx
 
 @app.callback(
     Output(component_id='button-column', component_property='children'), Input('model-train-btn', 'n_clicks'),
-    Input('model-data-btn', 'n_clicks'), Input('model-math-btn')
+    Input('model-data-btn', 'n_clicks'), Input('model-math-btn', 'n_clicks')
 )
-def btn_callback(btn_one, btn_two):
+def btn_callback(btn_one, btn_two, btn_three):
     if ctx.triggered_id is None:
         raise PreventUpdate
     else:
