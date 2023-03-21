36daf36407417e0f738a55dbd3e67390941155be
00XAH00 <pxaxulin@gmail.com>
2023-03-21 15:07:49 +0300

    add user upload file structure validate

--- web/callbacks/upload_file.py
+++ web/callbacks/upload_file.py
@@ -1,23 +1,26 @@
 from dash_extensions.enrich import html
 from dash_extensions.enrich import Input, Output, State, DashLogger
+from pandas.core.frame import DataFrame
 from app import app
+from services.file import UploadFile
 
 
 @app.callback(Output('output-image-upload', 'children'), Input('upload-image', 'contents'),
               State('upload-image', 'filename'), State('upload-image', 'last_modified'), log=True)
-def update_output(contents, name, dates, dash_logger: DashLogger):
-    if contents is not None:
-        dash_logger.info("Файл получен:)", autoClose=5000)
-        if not type_validate(name):
-            dash_logger.warning("Файл должен быть в формате csv!", autoClose=5000)
+def update_output(content: str, name: str, date: int, dash_logger: DashLogger):
+    if content is not None:
+        upload_file = UploadFile()
+
+        if not upload_file.type_validate(name):
+            dash_logger.warning("Файл должен быть в формате csv!", autoClose=2500)
+            return
+
+        input_data = upload_file.read_from_content(content)
+        if not isinstance(input_data, DataFrame):
+            dash_logger.error("Не удалось прочитать содержимое файла", autoClose=2500)
+            return
+
+        if not upload_file.table_structure_validate(input_data):
+            dash_logger.warning("Структура файла не соответствует примеру", autoClose=2500)
+
         return html.Div("test")
-
-
-def type_validate(file_name: str) -> bool:
-    split_name = file_name.split(".")
-    file_format = split_name[-1]
-
-    if file_format == "csv":
-        return True
-
-    return False
