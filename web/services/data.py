80343b1ae13fa6216157dadc661256fdc1abe2bc
00XAH00 <pxaxulin@gmail.com>
2023-03-22 17:09:40 +0300

    add send user data to backend server (now it save file on frontend server)

--- web/services/data.py
+++ web/services/data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from pandas import DataFrame
+from os import system
 
 
 class Data:
@@ -10,3 +12,8 @@ class Data:
         example_data = pd.read_csv('./data/car_prices_example.csv', parse_dates=True)
 
         return example_data
+
+    @staticmethod
+    def send_train_data(data: DataFrame):
+        system("rm ./data/train_user_data.csv")
+        data.to_csv('./data/train_user_data.csv')
