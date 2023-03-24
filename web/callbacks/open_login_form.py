5284b30213bad9a8c4197b134d929e9023f9b46c
00XAH00 <pxaxulin@gmail.com>
2023-03-24 07:39:31 +0300

    add auto auth if user-token in cookies

--- web/callbacks/open_login_form.py
+++ web/callbacks/open_login_form.py
@@ -1,7 +1,21 @@
-from dash import Output, State, Input
+import dash
+from dash_extensions.enrich import DashLogger
+from flask import request
+from dash import Output, State, Input, html
 from app import app
+from core.settings import settings
 
 
-@app.callback(Output("modal", "is_open"), Input("open-login-form", "n_clicks"), State("modal", "is_open"))
-def open_form(n1: int, is_open: bool):
-    return not is_open
+@app.callback(
+    [
+        Output("modal", "is_open"),
+        Output("login-btn-div", "children")
+    ],
+    Input("open-login-form", "n_clicks"), State("modal", "is_open"),
+    log=True
+)
+def open_form(n1: int, is_open: bool, dash_logger: DashLogger):
+    if not request.cookies.to_dict().get("user-token"):
+        return [not is_open, dash.no_update]
+    dash_logger.info("Вход выполнен успешно!", autoClose=settings.notify_auto_close_time)
+    return [dash.no_update, html.Div()]
