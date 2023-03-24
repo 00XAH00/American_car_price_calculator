d3de2947a2472a0ef7e5abd916a4c01f71633921
00XAH00 <pxaxulin@gmail.com>
2023-03-24 19:00:59 +0300

    now authorize class is inherited from server api class

--- web/services/Authorize.py
+++ web/services/Authorize.py
@@ -1,18 +1,16 @@
 from random import choice
+from flask import request
+from services.server import ServerApi
 
 
-class Authorize:
-    def __init__(self): ...
+class Authorize(ServerApi):
+    def __init__(self):
+        super().__init__()
+
+    def is_auth(self) -> bool:
+        token = request.cookies.to_dict().get("user-token")
+        return self.validate_token(token)
 
     @staticmethod
-    def auth(login: str, password: str):
-
-        if choice([0, 1]) == 1:
-            return False
-        result = ''.join([choice(login) for i in range(10)])
-        result += ''.join([choice(password) for i in range(20)])
-        result += ''.join([choice(login) for i in range(7)])
-        result += ''.join([choice(password) for i in range(3)])
-        result += ''.join([choice(login) for i in range(10)])
-
-        return result
+    def validate_token(token: str) -> bool:
+        return True
