274daaecd72d2fcd7faff3d62c98e7f8883e40e2
00XAH00 <pxaxulin@gmail.com>
2023-03-24 03:20:51 +0300

    add select type for input

--- web/page_elements/form/input.py
+++ web/page_elements/form/input.py
@@ -2,13 +2,14 @@ from dash_extensions.enrich import html
 import dash_bootstrap_components as dbc
 
 
-def form_input(input_id: str, placeholder: str) -> html.Div:
+def form_input(input_id: str, placeholder: str, input_type: str = "text") -> html.Div:
     return html.Div(
         [
             html.P(placeholder),
             dbc.Input(
                 placeholder=placeholder,
-                id=input_id
+                id=input_id,
+                type=input_type
             )
         ],
         className="modal-form-input"
