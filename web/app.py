a3889a9733653fbe7d4d04117de97814d3d754af
00XAH00 <pxaxulin@gmail.com>
2023-03-24 18:22:50 +0300

    add callbacks without Output

--- web/app.py
+++ web/app.py
@@ -1,9 +1,9 @@
 import dash_bootstrap_components as dbc
 from page_elements.main_page import layout
-from dash_extensions.enrich import DashProxy, LogTransform
+from dash_extensions.enrich import DashProxy, LogTransform, NoOutputTransform
 
 app = DashProxy(
-    transforms=[LogTransform()],
+    transforms=[LogTransform(), NoOutputTransform()],
     prevent_initial_callbacks=True,
     external_stylesheets=[
         dbc.themes.BOOTSTRAP,
