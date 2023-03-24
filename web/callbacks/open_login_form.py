d7cb9e5143617fa8696e20427b58e6f43e8842ee
00XAH00 <pxaxulin@gmail.com>
2023-03-24 03:19:44 +0300

    nothing

--- web/callbacks/open_login_form.py
+++ web/callbacks/open_login_form.py
@@ -2,8 +2,6 @@ from dash import Output, State, Input
 from app import app
 
 
-@app.callback(Output("modal", "is_open"),
-              Input("open-login-form", "n_clicks"),
-              State("modal", "is_open"))
-def open_form(n_clicks: int, is_open: bool):
+@app.callback(Output("modal", "is_open"), Input("open-login-form", "n_clicks"), State("modal", "is_open"))
+def open_form(n1: int, is_open: bool):
     return not is_open
