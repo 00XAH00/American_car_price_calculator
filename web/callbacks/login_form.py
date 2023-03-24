5e692dcb280588ff20a10db606b64fc8b4126ed7
00XAH00 <pxaxulin@gmail.com>
2023-03-24 07:08:53 +0300

    add write token in cookie

--- web/callbacks/login_form.py
+++ web/callbacks/login_form.py
@@ -1,4 +1,5 @@
-from dash import html
+import dash
+import flask
 from dash_extensions.enrich import Output, State, Input
 from dash_extensions.enrich import DashLogger
 from app import app
@@ -10,21 +11,22 @@ from services.Authorize import Authorize
     Output("login-btn-div", "children"),
     Input("login-btn", "n_clicks"),
     [
-      State("user-login", "value"),
-      State("user-password", "value"),
+        State("user-login", "value"),
+        State("user-password", "value"),
 
     ],
     log=True
 )
 def open_form(n1: int, user_login: str, user_password: str, dash_logger: DashLogger):
     authorize = Authorize()
+    # print(flask.request.cookies.to_dict())
 
     user_token = authorize.auth(user_login, user_password)
 
     if not user_token:
-
-        print(dash_logger)
         dash_logger.warning("Не верный логин или пароль")
         return login_form
+
+    dash.callback_context.response.set_cookie("token", user_token)
 
     return None
