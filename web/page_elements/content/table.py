6e6a0daecd1c6a78fadcbaf36c37c3b30da1af53
00XAH00 <pxaxulin@gmail.com>
2023-03-26 15:24:36 +0300

    change input table name in generate_table function

--- web/page_elements/content/table.py
+++ web/page_elements/content/table.py
@@ -3,11 +3,11 @@ from pandas import DataFrame
 from services.data import Data
 
 
-def generate_table(table: DataFrame = Data().get_example_table()):
+def generate_table(table_input: DataFrame = Data().get_example_table()):
     table = dash_table.DataTable(
         id="table",
-        data=table.to_dict('records'),
-        columns=[{"name": i, "id": i} for i in table.columns],
+        data=table_input.to_dict('records'),
+        columns=[{"name": i, "id": i} for i in table_input.columns],
         style_data={
             'backgroundColor': '#31302f',
             'color': 'white'
