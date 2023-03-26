997e1c19fe7215a3f84dbfd826e8817403a95a09
00XAH00 <pxaxulin@gmail.com>
2023-03-26 22:27:02 +0300

    add docker

--- web/app.py
+++ web/app.py
@@ -1,7 +1,10 @@
 import dash_bootstrap_components as dbc
+import flask
 from page_elements.main_page import layout
 from dash_extensions.enrich import DashProxy, LogTransform, NoOutputTransform
 
+
+app_server = flask.Flask(__name__)
 app = DashProxy(
     title="CarCalc",
     update_title="CarCalc updating>>>",
@@ -11,7 +14,8 @@ app = DashProxy(
     external_stylesheets=[
         dbc.themes.BOOTSTRAP,
         "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-    ]
+    ],
+    server=app_server
 )
 
 
